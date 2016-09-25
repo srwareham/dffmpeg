3e765f48f41b77f3c90323703a7bca259b853ffd
srwareham <srwareham@hotmail.com>
2016-09-25 13:04:10 -0400

    Created subdirectory for docker images

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-docker build -t arch-ffmpeg .
+# Change this value to the name of the image you would like to use
+image_name=arch-ffmpeg
+
+# docker container names cannot have whitespace
+sed -i -e 's/dffmpeg_image_name=.*/dffmpeg_image_name=$image_name/g' dffmpeg.sh
+
+cd images/$image_name
+./build.sh
+
